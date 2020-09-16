c06b5a54986ec5f88c3438b18f35aae5f7cfe364
Arron Cao <qqcao.cs@gmail.com>
2020-09-15 22:29:25 -0400

    update profile extraction

--- extract_profile.py
+++ extract_profile.py
@@ -1,33 +1,32 @@
-import pickle
+import argparse
+import parse
+from pathlib import Path
 
-with open('data/tx2-yolo-vqa-profile.pk', 'rb') as f:
-    # The protocol version used is detected automatically, so we do not
-    # have to specify it.
-    prof_data = pickle.load(f)
+_PREDICT_FILE = 'lxmert/src/tasks/lxmert_yolov5s_predict.py'
+_MODELING_FILE = '/home/tx2/work/AcceleratorBERT/lxmert/src/lxrt/modeling.py'
+components = {
+    'predict': _PREDICT_FILE,
+    'preprocess_image': _PREDICT_FILE,
+    'run_detection': _PREDICT_FILE,
+    'postprocess_feature': _PREDICT_FILE,
+    'image_encoder': _MODELING_FILE,
+    'ques_encoder': _MODELING_FILE,
+    'cross_encoder': _MODELING_FILE,
 
-timings = dict()
-for k, events in prof_data.items():
-    if not events:
-        continue
-    timings[k] = sum([e.total_average().cuda_time_total for e in events])
+}
 
-import csv
-with open('data/tx2-yolo-vqa-b1_n10.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    for k, v in timings.items():
-        writer.writerow(['/'.join(k), v/10000])
 
-from collections import defaultdict
-breakdown = defaultdict(list)
-for k, v in timings.items():
-    if k[1] == 'detection_model':
-        breakdown['detection_model'].append(v/1000)
-    if '/'.join(k).startswith('VQAModel/lxrt_encoder/model/bert/encoder/layer'):
-        breakdown['language_encoder'].append(v/1000)
-    if '/'.join(k).startswith('VQAModel/lxrt_encoder/model/bert/encoder/r_layers/'):
-        breakdown['vision_encoder'].append(v/1000)
-    if '/'.join(k).startswith('VQAModel/lxrt_encoder/model/bert/encoder/x_layers/'):
-        breakdown['cross_encoder'].append(v/1000)
+def main(args):
+    input_file = Path(args.input_file)
+    lines = input_file.read_text()
+    breakdowns = {k: next(parse.findall(
+        f"{k} ({v}) ({{:n}} samples", lines))[0] for k, v in components.items()}
+    for k, v in breakdowns.items():
+        print(f'{k}, {v}')
 
-for k, v in breakdown.items():
-    print(k, sum(v)/10)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", type=str,
+                        help="input svg file")
+    main(parser.parse_args())
